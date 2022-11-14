041ede30f50a361c52361904da71ec475bc55345
RuanCunha <ruanvcunha@hotmail.com>
2022-11-14 10:19:37 -0300

    Updates

---
+++ precache-manifest.9a888a94603af002e5d7d510da083166.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03195d4bf3b8e3c8761e38d3f43f4006",
+    "url": "/project-trivia-react-redux/index.html"
+  },
+  {
+    "revision": "321ba9332a0dd1d91776",
+    "url": "/project-trivia-react-redux/static/css/main.9b814562.chunk.css"
+  },
+  {
+    "revision": "2037ececb024eeb13cd9",
+    "url": "/project-trivia-react-redux/static/js/2.78d991e5.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/project-trivia-react-redux/static/js/2.78d991e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "321ba9332a0dd1d91776",
+    "url": "/project-trivia-react-redux/static/js/main.c5042f2c.chunk.js"
+  },
+  {
+    "revision": "20db37c26b020495b568",
+    "url": "/project-trivia-react-redux/static/js/runtime-main.d03c2dfb.js"
+  },
+  {
+    "revision": "d7a90d8c3bf21eb2035e6f0bf4ad2b11",
+    "url": "/project-trivia-react-redux/static/media/Black-cat-asks-logo.d7a90d8c.png"
+  },
+  {
+    "revision": "5f9233cefeb87a1df6653dc6985886fd",
+    "url": "/project-trivia-react-redux/static/media/play.5f9233ce.png"
+  }
+]);
